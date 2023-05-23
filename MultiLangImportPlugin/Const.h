664074a153b44bf234e3c7e27fe6cf71df005654
tsuge.kunio@clouds-inc.jp <tsuge.kunio@clouds-inc.jp>
2023-05-23 13:26:17 +0900

    下限SDKバージョンを修正

--- MultiLangImportPlugin/Const.h
+++ MultiLangImportPlugin/Const.h
@@ -3,8 +3,8 @@
 // This plugin requires SDK version 2.1.20.729 or later
 const int MIN_VER_A = 2;
 const int MIN_VER_B = 1;
-const int MIN_VER_C = 20;
-const int MIN_VER_D = 729;
+const int MIN_VER_C = 23;
+const int MIN_VER_D = 515;
 
 #define PLUGIN_NAME L"MultiLangImport"
 const char TEXT_MENU_NAME[] = "Multi-language Import";
